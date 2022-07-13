7be32732e5ffca35099330ceee15c12155d84e28
junior sousa <92957053+juniors0101@users.noreply.github.com>
2022-07-13 13:41:16 -0300

    up

---
+++ moments/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'moments/new', component: NewMomentComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
