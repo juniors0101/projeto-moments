27e760096b1f4632e58f76b3d1db031537749fc0
junior sousa <92957053+juniors0101@users.noreply.github.com>
2022-07-14 15:06:12 -0300

    up

---
+++ moments/src/app/Comment.ts
@@ -0,0 +1,8 @@
+export interface Comment {
+    id?: string;
+    text: string;
+    username: string;
+    mommentId: number;
+    Created_at?: string;
+    update_at?: string;
+}
--- moments/src/app/components/pages/new-moment/new-moment.component.ts
+++ moments/src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+
+import { MomentService } from 'src/app/services/moment.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewMomentComponent implements OnInit {
   btnText = 'Compartilhar!';
-  constructor() { }
+  
+  constructor(private momentService: MomentService) { }
 
   ngOnInit(): void {
+  }
+
+  async CreateHandler(moment: Moment){
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+
+    //todo
+
+    // enviar para o service
+
+    await this.momentService.createMoment(formData).subscribe();
+
+    //redirect
   }
 
 }
