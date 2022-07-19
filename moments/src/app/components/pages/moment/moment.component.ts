8d293bf530a46aa38ebbff119942c21a68dfad55
junior sousa <92957053+juniors0101@users.noreply.github.com>
2022-07-19 10:56:04 -0300

    up

--- moments/src/app/components/pages/moment/moment.component.ts
+++ moments/src/app/components/pages/moment/moment.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 import { Moment } from 'src/app/Moment';
 import { environment } from 'src/environments/environment';
 import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
@@ -17,7 +18,12 @@ export class MomentComponent implements OnInit {
   faTimes = faTimes;
   faEdit = faEdit;
 
-  constructor(private momentService: MomentService, private route: ActivatedRoute) { }
+  constructor(
+    private momentService: MomentService,
+    private route: ActivatedRoute,
+    private messagesService: MessagesService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
     //id que esta na url
@@ -28,4 +34,11 @@ export class MomentComponent implements OnInit {
     .subscribe((item) => (this.moment = item.data));
   }
 
+  async removeHandler(id: number) {
+    await this.momentService.removeMoment(id).subscribe();
+
+    this.messagesService.add("Momento excluído com sucesso!");
+
+    this.router.navigate(['/']);
+  }
 }
