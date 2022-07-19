a76b71bef56bdf9d6b5ae52e1925d72526dd49bc
junior sousa <92957053+juniors0101@users.noreply.github.com>
2022-07-19 14:05:43 -0300

    up

--- moments/src/app/app-routing.module.ts
+++ moments/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { EditMomentComponent } from './components/pages/edit-moment/edit-moment.component';
 import { MomentComponent } from './components/pages/moment/moment.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 import { AboutComponent } from './components/pages/about/about.component';
@@ -9,6 +10,7 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'moments/new', component: NewMomentComponent},
+  {path: 'moments/edit/:id', component: EditMomentComponent},
   {path: 'moments/:id', component: MomentComponent},
 ];
 
